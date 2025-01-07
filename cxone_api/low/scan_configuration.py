1ad3455f38462bc7b48a850ac6a9368b99241759
nleach999 <nleach999@gmail.com>
2025-01-07 11:17:39 -0600

    add missing undocumented api

--- cxone_api/low/scan_configuration.py
+++ cxone_api/low/scan_configuration.py
@@ -10,7 +10,13 @@ async def retrieve_tenant_configuration(client : CxOneClient) -> requests.Respon
 
 async def update_tenant_configuration(client : CxOneClient, configs : List[dict]) -> requests.Response:
     url = urljoin(client.api_endpoint, "configuration/tenant")
-    return await client.exec_request(requests.patch, url, body=configs)
+    return await client.exec_request(requests.patch, url, json=configs)
+
+@dashargs("config-keys")
+async def delete_tenant_configuration(client : CxOneClient, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, "configuration/tenant")
+    url = join_query_dict(url, kwargs)
+    return await client.exec_request(requests.delete, url)
 
 @dashargs("project-id")
 async def retrieve_project_configuration(client : CxOneClient, **kwargs) -> requests.Response:
@@ -22,7 +28,7 @@ async def retrieve_project_configuration(client : CxOneClient, **kwargs) -> requ
 async def update_project_configuration(client : CxOneClient, configs : List[dict], **kwargs) -> requests.Response:
     url = urljoin(client.api_endpoint, "configuration/project")
     url = join_query_dict(url, kwargs)
-    return await client.exec_request(requests.patch, url)
+    return await client.exec_request(requests.patch, url, json=configs)
 
 @dashargs("config-keys", "project-id")
 async def delete_project_configuration(client : CxOneClient, **kwargs) -> requests.Response:
