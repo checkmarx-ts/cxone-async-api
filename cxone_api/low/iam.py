bfa6fe1ab7faca87ea77880f06b84718e7f3dd7f
nleach999 <nleach999@gmail.com>
2025-01-21 12:14:18 -0600

    fix path to version

--- cxone_api/low/iam.py
+++ cxone_api/low/iam.py
@@ -3,7 +3,7 @@ from requests.compat import urljoin
 from ..util import join_query_dict
 from ..client import CxOneClient
 from deprecation import deprecated
-from __version__ import __version__
+from ..__version__ import __version__
 
 @deprecated("1.0.3", current_version=__version__, details="Use access_mgmt.user_mgmt.retrieve_groups")
 async def retrieve_groups(client : CxOneClient, **kwargs) -> requests.Response:
