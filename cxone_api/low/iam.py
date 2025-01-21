f55c72eb91a0f5e99e5a73b2890127393423b35f
nleach999 <nleach999@gmail.com>
2025-01-21 11:54:46 -0600

    mark undocumented api as deprecated

--- cxone_api/low/iam.py
+++ cxone_api/low/iam.py
@@ -2,7 +2,10 @@ import requests
 from requests.compat import urljoin
 from ..util import join_query_dict
 from ..client import CxOneClient
+from deprecation import deprecated
+from __version__ import __version__
 
+@deprecated("1.0.3", current_version=__version__, details="Use access_mgmt.user_mgmt.retrieve_groups")
 async def retrieve_groups(client : CxOneClient, **kwargs) -> requests.Response:
     url = join_query_dict(urljoin(client.admin_endpoint, "groups"), kwargs)
     return await client.exec_request(requests.get, url)
