bafaa272dd28e849359f0cfd3727f4d0cceca320
nleach999 <nleach999@gmail.com>
2025-02-24 12:32:40 -0600

    remove invalid apis

--- tests/test_low_access_mgmt.py
+++ tests/test_low_access_mgmt.py
@@ -7,9 +7,6 @@ class TestLowAccessMgmt(BaseTest):
     async def test_canary(self):
         self.assertTrue(1 == 1)
 
-    async def test_retrieve_users_groups(self):
-        await self.execute_client_call(um.retrieve_users_groups, self.assert_response_ok)
-    
     async def test_retrieve_groups(self):
         await self.execute_client_call(um.retrieve_groups, self.assert_response_ok)
 
