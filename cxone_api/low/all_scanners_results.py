55bc37417aad372de1f8c7b0acbdea387e7d75c3
nleach999 <nleach999@gmail.com>
2025-02-24 12:02:01 -0600

    additional api support

---
+++ cxone_api/low/all_scanners_results.py
@@ -0,0 +1,10 @@
+import requests
+from cxone_api import CxOneClient
+from cxone_api.util import join_query_dict, dashargs
+from requests.compat import urljoin
+
+@dashargs("scan-id", "exclude-result-types")
+async def retrieve_scan_results_all_scanners(client : CxOneClient, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, "results")
+    url = join_query_dict(url, kwargs)
+    return await client.exec_request(requests.get, url)
--- cxone_api/low/api.py
+++ cxone_api/low/api.py
@@ -1,3 +1,4 @@
+import urllib.parse
 import requests
 from cxone_api import CxOneClient
 from cxone_api.util import join_query_dict, dashargs
@@ -10,5 +11,5 @@ async def retrieve_apisec_security_risks(client : CxOneClient, scan_id : str, **
     return await client.exec_request(requests.get, url)
 
 async def retrieve_risk_details(client : CxOneClient, risk_id : str) -> requests.Response:
-    url = urljoin(client.api_endpoint, f"apisec/static/api/risks//risk/{risk_id}")
+    url = urljoin(client.api_endpoint, f"apisec/static/api/risks/risk/{risk_id}")
     return await client.exec_request(requests.get, url)
