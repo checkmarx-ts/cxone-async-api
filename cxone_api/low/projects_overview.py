65dbb4ebfaaa93937a15a7e8af631d17d61fa3bc
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-10-30 14:09:46 -0500

    add projects overview

---
+++ cxone_api/low/projects_overview.py
@@ -0,0 +1,16 @@
+import requests
+from requests.compat import urljoin
+from ..util import dashargs, join_query_dict
+from ..client import CxOneClient
+
+@dashargs("from-date", "group-ids", "risk-level", "scan-origin", "source-type", "tag-keys", "tag-values", "to-date")
+async def retrieve_projects_overview(client : CxOneClient, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, "projects-overview")
+    url = join_query_dict(url, kwargs)    
+    return await client.exec_request(requests.get, url)
+
+@dashargs("from-date", "group-ids", "risk-level", "scan-origin", "source-type", "tag-keys", "tag-values", "to-date", "group-by-field")
+async def retrieve_aggregated_results(client : CxOneClient, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, "projects-overview/aggregate")
+    url = join_query_dict(url, kwargs)    
+    return await client.exec_request(requests.get, url)
