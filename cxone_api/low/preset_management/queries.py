e61a4960b35f2b47a98a402b6d55e7bcd3d0f516
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-10-30 14:10:53 -0500

    handle presets

---
+++ cxone_api/low/preset_management/queries.py
@@ -0,0 +1,16 @@
+import requests
+from requests.compat import urljoin
+from cxone_api.util import dashargs, join_query_dict
+from cxone_api.client import CxOneClient
+
+@dashargs("search-term")
+async def retrieve_list_of_query_families (client : CxOneClient, scanner : str, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, f"preset-manager/{scanner}/query-families")
+    url = join_query_dict(url, kwargs)    
+    return await client.exec_request(requests.get, url)
+
+@dashargs("search-term")
+async def retrieve_list_of_queries_in_a_family (client : CxOneClient, scanner : str, family : str, **kwargs) -> requests.Response:
+    url = urljoin(client.api_endpoint, f"preset-manager/{scanner}/query-families/{family}/queries")
+    url = join_query_dict(url, kwargs)    
+    return await client.exec_request(requests.get, url)
