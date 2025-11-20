26239358131e9f428ad62e3c424a4bb55744bf65
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-11-20 12:01:28 -0600

    sphinx docs config

---
+++ docs/conf.py
@@ -0,0 +1,41 @@
+from cxone_api.__version__ import __version__
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'cxone-async-api'
+copyright = '2025, Checkmarx'
+author = 'Nathan Leach, OSCP, CSSLP, Principal Solutions Architect'
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'autoapi.extension'
+]
+
+autoapi_dirs=["../cxone_api"]
+autoapi_member_order="alphabetical"
+autoapi_options=['members','inherited-members','undoc-members','show-module-summary']
+autoapi_ignore=["cxone_api/__version__.py", "*exceptions*", "cxone_api/client.py"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+latex_elements = {
+  "makeindex" : r"\usepackage[columns=1]{idxlayout}\makeindex"
+}
