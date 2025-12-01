15fbcd42f1a97361affc617b17c7d2488fecc016
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2025-12-01 05:51:00 -0600

    documentation

--- cxone_api/high/util.py
+++ cxone_api/high/util.py
@@ -15,10 +15,29 @@ class CxOneVersions:
 
   @staticmethod
   async def factory(client : CxOneClient):
+    """Creates an instance of CxOneVersions
+
+        :param client: The CxOneClient instance used to communicate with Checkmarx One
+        :type client: CxOneClient
+
+        :rtype: CxOneVersions
+    
+    """
     v = json_on_ok(await retrieve_versions(client))
     return CxOneVersions(CxOne=v["CxOne"], SAST=v["SAST"], KICS=v["KICS"])
 
 async def scan_inspector_factory (client : CxOneClient, scanid : str) -> ScanInspector:
+    """Creates an instance of ScanInspector for the provided Scan Id
+
+        :param client: The CxOneClient instance used to communicate with Checkmarx One
+        :type client: CxOneClient
+
+        :param scanid: The scan id to be inspected.
+        :type scanid: str
+
+        :rtype: ScanInspector
+        
+    """
     scan = json_on_ok(await retrieve_scan_details(client, scanid))
     return ScanInspector(scan)
 
