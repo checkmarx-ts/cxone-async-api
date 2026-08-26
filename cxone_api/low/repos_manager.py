6478733da2cfb8d21421a90220267d03f3993e00
cx-nathan-leach <203706735+cx-nathan-leach@users.noreply.github.com>
2026-08-26 11:18:10 -0500

    add config update ability

--- cxone_api/low/repos_manager.py
+++ cxone_api/low/repos_manager.py
@@ -1,5 +1,7 @@
 import requests
+from typing import Dict
 from requests.compat import urljoin
+from ..util import join_query_dict
 from ..client import CxOneClient
 
 async def retrieve_repo_by_id(client : CxOneClient, repoid : str) -> requests.Response:
@@ -7,6 +9,11 @@ async def retrieve_repo_by_id(client : CxOneClient, repoid : str) -> requests.Re
     url = urljoin(client.api_endpoint, f"repos-manager/repo/{repoid}")
     return await client.exec_request(requests.get, url)
 
+async def update_repo_by_id_for_project(client : CxOneClient, repoid : str, projectid : str, payload : Dict) -> requests.Response:
+    """|LowLevelApiDocstring|"""
+    url = urljoin(client.api_endpoint, f"repos-manager/repo/{repoid}")
+    url = join_query_dict(url, {"projectId" : projectid})
+    return await client.exec_request(requests.put, url, json=payload)
 
 async def get_scm_by_id(client : CxOneClient, scmId : str) -> requests.Response:
     """|LowLevelApiDocstring|"""
